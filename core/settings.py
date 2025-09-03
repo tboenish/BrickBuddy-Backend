eddc1d235dec22b9284a4e0ddac0f93960140980
Thomas Boenish <tboenish@gmail.com>
2025-09-02 17:43:15 -0700

    chore: setup postgres connection

--- core/settings.py
+++ core/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR/".env")
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'brickbuddy',
 
     #third part apps
     "rest_framework",
@@ -88,8 +92,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME",''),
+        'USER': os.getenv("DB_USER",''),
+        'PASSWORD':os.getenv('DB_PASSWORD',''),
+        'HOST':os.getenv('DB_HOST','localhost'),
+        'PORT':os.getenv('DB_PORT','5432'),
     }
 }
 
