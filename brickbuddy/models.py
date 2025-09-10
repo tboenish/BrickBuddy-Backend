b152ed71ca431bf63bd1f2b7ad5a1504417d56cf
tboenish <154274875+tboenish@users.noreply.github.com>
2025-09-09 19:44:54 -0700

    Chore/models (#1)
    * added Set and Theme models
    
    * chore: added more models and made initial migration to Postgres

--- brickbuddy/models.py
+++ brickbuddy/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+#Theme model
+class Theme(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    #id = models.IntegerField(unique=True)
+
+
+    #Meta controls characteristics of the data. For example: adding indexes or ordering by name
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name'])
+        ]
+
+    #Adding __str__ allows the model to read as a string instead of an object
+    def __str__(self):
+        return self.name
+
+
+#Set model
+class Set(models.Model):
+    set_num = models.CharField(unique=True)
+    name = models.CharField(max_length=120)
+    year = models.IntegerField()
+    num_parts = models.IntegerField()
+    theme = models.ForeignKey(
+        Theme, on_delete=models.PROTECT,related_name="sets"
+    )
+
+    class Meta:
+        ordering = ['-year','name']
+        indexes = [
+            models.Index(fields=['year']),
+            models.Index(fields=['name']),
+            models.Index(fields=['theme','year']),
+        ]
+    
+    def __str__(self):
+        return f"{self.set_num}-{self.name}"
+
+#Minifigure Model
+class MiniFig(models.Model):
+    fig_num = models.CharField(unique=True)
+    name = models.CharField(max_length=120)
+    num_parts = models.IntegerField()
+
+    class Meta:
+        ordering = ['fig_num']
+        indexes = [
+            models.Index(fields=['fig_num']),
+        ]
+    
+    def __str__(self):
+        return f"{self.fig_num}-{self.name}"
+
+
+
